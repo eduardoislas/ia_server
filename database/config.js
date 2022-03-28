6cc2849c4700e62f2ddbf5e0b5158e4ab353addb
eduardoislas <eislas22@gmail.com>
2022-03-28 11:53:14 -0700

    Backend autenticación

---
+++ database/config.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+
+    try {
+        mongoose.connect ( process.env.DB_CNN)
+
+        console.log('DB Online')
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error en la Base de datos');
+    }
+}
+
+
+        
+module.exports = {
+    dbConnection
+}
